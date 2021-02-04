dfd282f23556e45ff06f4a098f47500ce0c634e2
Chris Jensen <2920476+chrisjensen@users.noreply.github.com>
2021-02-04 15:00:43 +1300

    Fix input to glob

--- src/actions/campaigns.js
+++ src/actions/campaigns.js
@@ -33,7 +33,9 @@ export async function buildStyles(filename, config) {
 	const stylesDir = path.join(process.cwd(), "stylesheets");
 	const filePath = filename.split(path.sep)[0];
 
-	const files = await glob(`${path.join(stylesDir, filePath)}/**/*.scss`);
+	// glob requires posix paths, so in case we're running on windows
+	// we need to replace \ with /
+	const files = await glob(`${path.join(stylesDir, filePath).replace('\\', '/')}/**/*.scss`);
 
 	const configFiles = {};
 	for (const file of files) {
